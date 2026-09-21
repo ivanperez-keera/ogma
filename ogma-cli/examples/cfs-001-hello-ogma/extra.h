5f216fc4a3b151fd1f10697e9ed14c8d91649640
Ivan Perez <ivan.perezdominguez@nasa.gov>
2026-09-21 00:33:32 +0000

    Modify example to publish result

--- ogma-cli/examples/cfs-001-hello-ogma/extra.h
+++ ogma-cli/examples/cfs-001-hello-ogma/extra.h
@@ -1,4 +1,5 @@
-#define SAMPLE_MID 0x1878
+#define SAMPLE_MID                      0x1878
+#define COPILOT_HANDLERTESTOGMA_TLM_MID 0x0878
 
 typedef struct sample_msg {
    CFE_MSG_CommandHeader_t CmdHeader;
